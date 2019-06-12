60a929026942d4067c48a0a8cfdec591a68d38ff
Sala 310 <sala310@poliacademico.edu.loc>
2019-06-12 12:26:39 -0500

    Retos actualizados

---
+++ retos-js/5-reto.js
@@ -0,0 +1,18 @@
+/* initializate 
+these three variables
+*/
+var a;
+var b;
+var c;
+
+a = 5;
+b = 10;
+c = "I am a";
+
+/* do not change code 
+below this line
+*/
+
+a = a + 1;
+b = b + 5;
+c = c + " String! ";
