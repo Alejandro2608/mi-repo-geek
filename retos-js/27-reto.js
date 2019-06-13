a865a24d0c60074aff0c8bd701776acafdeb05bf
Alejandro Ortega <alejo.ortegaa@gmail.com>
2019-06-13 14:14:34 -0500

    Retos actualizados

---
+++ retos-js/27-reto.js
@@ -0,0 +1,7 @@
+// example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is" + ourName + ", how are you";
+
+// Only change code below this line
+var myName = "Tajada67890";
+var myStr = "My name is" + myName + " and i am well!";
