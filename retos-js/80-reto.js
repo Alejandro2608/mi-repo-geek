d2515acc0339cc6fc0bd42e1501571ff84bc3a13
Alan Bedoya <alanmbc1889@outlook.com>
2019-06-22 12:48:37 -0500

    retos actualizados

---
+++ retos-js/80-reto.js
@@ -0,0 +1,15 @@
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+    "name": "Kyūbi",
+    "legs": 3,
+    "tails": 9,
+    "friends": ["hello boy"]
+};
