b4df1e943e69793240f07da6f44b6ef70669235a
Alejandro Ortega <alejo.ortegaa@gmail.com>
2019-06-24 22:08:31 -0500

    retos actualizados

---
+++ retos-js/89-reto.js
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [ 
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    // Add record here
+    {
+      "artist": "Dante SP",
+  "title": "The less i know the better",
+  "release_year": 1983,
+  "formats": [
+    "CP",
+    "3D",
+    "MP3"]
+  
+    }
+  ];
