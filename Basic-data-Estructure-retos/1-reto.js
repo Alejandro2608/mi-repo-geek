492050c7390455909736bc9e30ea6cc65366c814
Sala 310 <sala310@poliacademico.edu.loc>
2019-07-08 12:58:07 -0500

    Basic structure

---
+++ Basic-data-Estructure-retos/1-reto.js
@@ -0,0 +1,2 @@
+let yourArray = ['one', 80, 'four', true, null]; // change this line
+console.log(yourArray.length);
---
+++ Basic-data-Estructure-retos/2-reto.js
@@ -0,0 +1,9 @@
+let myArray = ["a", "b", "c", "d"];
+// change code below this line
+myArray[0] = "a";
+myArray[1] = "not b anymore";
+myArray[2] = "c";
+myArray[3] = "d";
+
+//change code above this line
+console.log(myArray);
